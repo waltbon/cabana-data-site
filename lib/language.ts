1138e65e285fa1d21c84ab5d3d55ec279dc251f2
Walter Bonilla <wbonillacr@gmail.com>
2020-06-01 07:55:17 -0600

    Lang fixes 2

--- lib/language.ts
+++ lib/language.ts
@@ -4,15 +4,24 @@ export const getRequestLanguage = (req, i18n) => {
     let lang = null;
 
     if (req) {
-        lang = req['language'] ? req['language'] : i18n.language;
+        lang = req['language'];
     }
 
-    if (!i18n.language) {
-        i18n.changeLanguage(lang)
+    if (!lang && i18n) {
+        lang = i18n.language;
     }
 
+    if (!lang) {
+        lang = 'es';
+    }
+    
     if (lang && lang.length > 2) {
         lang = lang.substring(0,2);
     }
+    
+    if(!i18n.language) {
+        i18n.changeLanguage(lang);
+    }
+
     return lang;
 }
